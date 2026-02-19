8b5c716aa8eacb437108fb842ba00f0d778ed90a
mot566 <mo@vorm-walde.de>
2026-02-19 10:21:43 +0100

    Update app name to include language designation

--- adobe_adobereader-de/BuildTurboImage.ps1
+++ adobe_adobereader-de/BuildTurboImage.ps1
@@ -39,7 +39,7 @@ if (-not $elevated) {
 $HubOrg = (Split-Path $scriptPath -Leaf) -replace '_', '/' # Set the repo name based on the folder path of the script assuming the folder is vendor_appname
 $Vendor = "Adobe"
 $AppDesc = "View, print, search, sign, verify, and collaborate on PDF documents."
-$AppName = "Acrobat Reader"
+$AppName = "Acrobat Reader (de)"
 $VendorURL = "https://adobe.com"
 
 ########################################
